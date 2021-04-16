6e940d08eaa418be04cf46284fcb7426f089ca08
Vikash kumar <sushilraj176@gmail.com>
2021-04-16 10:25:15 +0530

    Updates

---
+++ precache-manifest.c171e11f8b7e632746865b486e6b5028.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0a63a9f1bd84e74dbbab943b3089ee",
+    "url": "/todoreactapp/index.html"
+  },
+  {
+    "revision": "f93ab94f88859823ff69",
+    "url": "/todoreactapp/static/css/main.3cbf75b4.chunk.css"
+  },
+  {
+    "revision": "6b5bf8ebab56f9223c33",
+    "url": "/todoreactapp/static/js/2.723d40da.chunk.js"
+  },
+  {
+    "revision": "f93ab94f88859823ff69",
+    "url": "/todoreactapp/static/js/main.0b5204fd.chunk.js"
+  },
+  {
+    "revision": "984904d36a2775ad595a",
+    "url": "/todoreactapp/static/js/runtime~main.db4ebe41.js"
+  }
+]);
